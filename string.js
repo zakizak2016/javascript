a98abd2cac8bd33efb2bc79bbbeb2909314fff95
zaki <zakizak2016@yahoo.com>
2016-02-17 16:49:16 +0700

    Update string.js

--- string.js
+++ string.js
@@ -1,5 +1,4 @@
-charAt()
-charCodeAt()
+
 
 /* concat() */ 
 /* combine two or more string object */
@@ -8,9 +7,14 @@ var str2 = "world!";
 var res = str1.concat(str2);
 // => Hello words !
 
+/* toUpperCase() */ 
+/* make capitalize */
+var str = "Hello World!";
+var res = str.toUpperCase();
+// => HELLO WORLD!
 
-
-
+charAt()
+charCodeAt()
 
 endsWith()
 fromCharCode()
@@ -33,11 +37,7 @@ toLowerCase()
 toString()
 
 
-/* toUpperCase() */ 
-/* make capitalize */
-var str = "Hello World!";
-var res = str.toUpperCase();
-// => HELLO WORLD!
+
 
 trim()
 valueOf()
