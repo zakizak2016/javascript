32d6703250b2e3489ad9e2cc6222ec47b534b4c2
zaki <zakizak2016@yahoo.com>
2016-02-17 16:47:09 +0700

    Update string.js

--- string.js
+++ string.js
@@ -1,1 +1,36 @@
+charAt()
+charCodeAt()
 
+/* concat() */ 
+/* combine two or more string object */
+var str1 = "Hello ";
+var str2 = "world!";
+var res = str1.concat(str2);
+// => Hello words !
+
+
+
+
+
+endsWith()
+fromCharCode()
+includes()
+indexOf()
+lastIndexOf()
+localeCompare()
+match()
+repeat()
+replace()
+search()
+slice()
+split()
+startsWith()
+substr()
+substring()
+toLocaleLowerCase()
+toLocaleUpperCase()
+toLowerCase()
+toString()
+toUpperCase()
+trim()
+valueOf()
